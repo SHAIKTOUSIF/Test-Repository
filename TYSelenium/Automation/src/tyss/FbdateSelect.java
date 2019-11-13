2636c4fbd534ee0e977b451b28554fd4f891307e
tousif <tousif@LAPTOP-P04MERIM>
2019-11-13 17:31:50 +0530

    fb

--- TYSelenium/Automation/src/tyss/FbdateSelect.java
+++ TYSelenium/Automation/src/tyss/FbdateSelect.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class Fbdate {
+public class FbdateSelect {
 	static {
 		System.setProperty("webdriver.chrome.driver","./Drivers/ChromeDriver.exe");
 	}
